e752421c16d087b7727f207d98f3d423fd5b6884
dawn_scroll <dibyomails007gmail.com>
2016-11-05 15:30:44 +0530

    piping and responsing to server

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-// creatng a server
+// creatng a server and piping the readme to response
 
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res){
    console.log('req was made : ' + req.url);
-   res.writeHead(200,{'content': 'text/plain'});   // 200 status code
-   res.end('hello guys');
+   var myReadStream = fs.createReadStream(__dirname +'/readit.txt', 'utf8');
+
+      res.writeHead(200,{'content': 'text/plain'});    // 200 status code
+   myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
