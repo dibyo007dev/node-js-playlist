838140bc5de7c1c2464af0eeaeb5c38a894df7f1
dawn_scroll <dibyomails007gmail.com>
2016-11-07 00:23:38 +0530

    templating with ejs

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('this is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-  res.send('this is the contact page');
+  res.sendFile(__dirname + '/contacts.html');
+});
+
+app.get('/profile/:name', function(req, res){
+  var data = {age: 29, job: 'coder'};
+    res.render('profile', {person: req.params.name, data: data});
 });
 
 app.listen(3000);
