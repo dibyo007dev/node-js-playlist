55c5f0a6489fe0d0c175e3aee18fd559a6b3dd6b
dawn_scroll <dibyomails007gmail.com>
2016-11-07 23:28:54 +0530

    done with partial templating

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', function());
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function(req, res){
-  res.sendFile(__dirname + '/contacts.html');
+  res.render('contacts');
 });
 
 app.get('/profile/:name', function(req, res){
-  var data = {age: 29, job: 'coder'};
+  var data = {age: 29, job: 'coder', hobbies: ['eating', 'fighting', 'coding']};
     res.render('profile', {person: req.params.name, data: data});
 });
 
