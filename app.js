e89a631a78026d1089d4d422bce8b088245f6fa5
dawn_scroll <dibyomails007gmail.com>
2016-11-08 03:20:08 +0530

    handling post request

--- app.js
+++ app.js
@@ -2,17 +2,28 @@ var express = require('express');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('assets')
-);
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
   res.render('index');
 });
 
 app.get('/contact', function(req, res){
-  res.render('contacts');
+  console.log(req.query);
+  res.render('contacts', {qs: req.query});
 });
+
+app.post('/contact', urlencodedParser, function(req, res){
+  console.log(req.query);
+  console.log(req.body);
+  res.render('contact-success', {data: req.body});
+});
+
 
 app.get('/profile/:name', function(req, res){
   var data = {age: 29, job: 'coder', hobbies: ['eating', 'fighting', 'coding']};
